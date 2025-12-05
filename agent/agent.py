9579907aad4a4a7ab949dca4a35d2592a62d8a6b
zakariaaithssain <zakariaithssain@gmail.com>
2025-12-05 22:48:34 +0100

    implement the model's tool calls executor

--- agent/agent.py
+++ agent/agent.py
@@ -1,7 +1,7 @@
+import json 
 from openai import OpenAI
 
-from config.model_config import system_prompt
-
+from config.model_config import system_prompt, TOOLS, FUNCTIONS
 
 class Agent: 
     def __init__(self, base_url : str, api_key : str, model : str):
@@ -10,11 +10,23 @@ class Agent:
 
 
     def chat(self, message : str):
-        responses = self.client.chat.completions.create(
+        response = self.client.chat.completions.create(
                 model = self.model, 
                 messages = [{"role" : "system", "content" : system_prompt}, 
                             {"role" : "user", "content" : message}], 
+                tools= TOOLS, 
                 temperature = 0  #as the task is deterministic.                          
             )
-        return responses.choices[0].message["content"]
+        #TODO: implement tool calling flow
+        pass
+
+    
+    def execute_tool_call(self, tool_call):
+        """Execute a Model's tool call and return the result."""
+        fn_name = tool_call.function.name
+        args = json.loads(tool_call.function.arguments)
+        
+        python_fn = FUNCTIONS[fn_name]
+        result = python_fn(**args)
+        return result
 
--- config/model_config.py
+++ config/model_config.py
@@ -4,3 +4,12 @@ model = "qwen/qwen3-32b"
 
 #to prompt engineer the model for this specific project.
 system_prompt = """ """
+
+#tools that will be defined in sheets/tools.py
+#the best thing to do is to create these tools based on the gspread API docs: 
+# https://docs.gspread.org/en/latest/
+
+TOOLS = []
+
+#map tool names to functions objects
+FUNCTIONS = {}
