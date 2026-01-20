df49fe9015f94a6964f1a43fa267db966dae7ec4
zakariaaithssain <zakariaithssain@gmail.com>
2026-01-20 22:39:01 +0100

    initial migration to langchain instead of reinventing the wheel

--- agent/agent.py
+++ agent/agent.py
@@ -1,45 +1,76 @@
-import json 
-from openai import OpenAI
+from langchain.agents import create_agent
 
-from config.model_config import SYSTEM_PROMPT, FUNCTIONS_DEF, FUNCTIONS_MAP
+#env variables are loaded inside model_config, so importing it would load them and Langchain detects them
+import config.model_config as conf
 
 
-#TODO: add streaming
+agent = create_agent(model=conf.MODEL_PROVIDER,
+                      system_prompt=conf.SYSTEM_PROMPT,
+                      tools=conf.TOOLS,
+                        )
 
-class Agent: 
-    def __init__(self, base_url : str, api_key : str, model : str):
-        self.client = OpenAI(base_url=base_url, api_key=api_key)
-        self.model = model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#deprecated (used befor I decided to use langchain)
+
+# import json 
+# from openai import OpenAI
+
+# from config.model_config import SYSTEM_PROMPT, FUNCTIONS_DEF, FUNCTIONS_MAP
+
+
+
+# class Agent: 
+#     def __init__(self, base_url : str, api_key : str, model : str):
+#         self.client = OpenAI(base_url=base_url, api_key=api_key)
+#         self.model = model
         
 
 
-    def chat(self, prompt : list[dict]):
-        context = [{"role" : "system", "content" : SYSTEM_PROMPT}]
-        context += prompt
-        while True:
-            response = self.client.chat.completions.create(
-                    model = self.model, 
-                    messages = context, 
-                    functions = FUNCTIONS_DEF, 
-                    temperature = 0  #as the task is deterministic.                          
-                )
+#     def chat(self, prompt : list[dict]):
+#         context = [{"role" : "system", "content" : SYSTEM_PROMPT}]
+#         context += prompt
+#         while True:
+#             response = self.client.chat.completions.create(
+#                     model = self.model, 
+#                     messages = context, 
+#                     functions = FUNCTIONS_DEF, 
+#                     temperature = 0  #as the task is deterministic.                          
+#                 )
             
-            msg = response.choices[0].message
-            context.append(msg)
+#             msg = response.choices[0].message
+#             context.append(msg)
 
-            function_calls = msg.tool_calls
-            if function_calls: 
-                for call in function_calls:
-                    if call.type == 'function':
-                        result = self.execute_fn_call(call)
-                        #add function calls results to 'context' 
-                        context.append({
-                                "role": "function",
-                                "name": call.function.name,
-                                "content": json.dumps(result)})
-                continue
-            else: 
-                return msg.content
+#             function_calls = msg.tool_calls
+#             if function_calls: 
+#                 for call in function_calls:
+#                     if call.type == 'function':
+#                         result = self.execute_fn_call(call)
+#                         #add function calls results to 'context' 
+#                         context.append({
+#                                 "role": "function",
+#                                 "name": call.function.name,
+#                                 "content": json.dumps(result)})
+#                 continue
+#             else: 
+#                 return msg.content
             
                 
                 
@@ -47,13 +78,13 @@ class Agent:
 
 
     
-    def execute_fn_call(self, fn_call):
-        """Execute a Model's function call and return the result."""
-        #this parsing is based on the structure of the model's return (ChatCompletionMessageToolCallUnion)
-        fn_name = fn_call.function.name
-        args = json.loads(fn_call.function.arguments)
+#     def execute_fn_call(self, fn_call):
+#         """Execute a Model's function call and return the result."""
+#         #this parsing is based on the structure of the model's return (ChatCompletionMessageToolCallUnion)
+#         fn_name = fn_call.function.name
+#         args = json.loads(fn_call.function.arguments)
         
-        python_fn = FUNCTIONS_MAP[fn_name]
-        result = python_fn(**args)
-        return result
+#         python_fn = FUNCTIONS_MAP[fn_name]
+#         result = python_fn(**args)
+#         return result
 
--- main.py
+++ main.py
@@ -1,30 +1,55 @@
-from google.auth.exceptions import RefreshError
-from agent.agent import Agent
-from config.model_config import model_base_url, model_name, model_api_key
 
-#TODO: add loggings everywhere
+from agent.agent import agent
 
-agent = Agent(base_url=model_base_url, model=model_name, api_key= model_api_key)
+response = agent.invoke(input={
+    'messages': [
+        {'role': 'user', 'content': 'hi'}
+        ]
+    }
+    )
 
-context = []
 
-try: 
-    while True:
-        message = str(input("User: "))
-        print(f"{100*'-'}")
-        context.append({"role" : "user", "content" : message})
-        full_response = ""
+print(response["messages"][-1].content)
 
-        #stream
-        print("GestAI: ",end="")
-        for chunk in agent.chat(prompt= context):
-            print(chunk, end="", flush=True)
-            full_response += chunk
 
-        print(f"\n{100*'-'}")
-        context.append({"role" : "assistant", "content": full_response})
 
-except KeyboardInterrupt: 
-    print("\nchat terminated.")
-except RefreshError: 
-    print("\n The Oauth is expired, run the following script and try again: rm -rf ~/.config/gspread")
+
+
+
+
+
+
+
+
+#DEPRECATED (I SWITCHED TO LANGCHAIN THAT WOULD HANDLE ALL THIS FOR ME)
+
+# from google.auth.exceptions import RefreshError
+# from agent.agent import Agent
+# from config.model_config import model_base_url, model_name, model_api_key
+
+
+
+# agent = Agent(base_url=model_base_url, model=model_name, api_key= model_api_key)
+
+# context = []
+
+# try: 
+#     while True:
+#         message = str(input("User: "))
+#         print(f"{100*'-'}")
+#         context.append({"role" : "user", "content" : message})
+#         full_response = ""
+
+#         #stream
+#         print("GestAI: ",end="")
+#         for chunk in agent.chat(prompt= context):
+#             print(chunk, end="", flush=True)
+#             full_response += chunk
+
+#         print(f"\n{100*'-'}")
+#         context.append({"role" : "assistant", "content": full_response})
+
+# except KeyboardInterrupt: 
+#     print("\nchat terminated.")
+# except RefreshError: 
+#     print("\n The Oauth is expired, run the following script and try again: rm -rf ~/.config/gspread")
