f0205d6348145886ee57e57907f82e555192ee47
zakariaaithssain <zakariaithssain@gmail.com>
2026-02-13 15:31:17 +0100

    refine env loading

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+
+from dotenv import load_dotenv
+from pathlib import Path
+import google.auth.exceptions
+
+import logging
 import warnings
 
 warnings.filterwarnings(
@@ -7,28 +13,28 @@ warnings.filterwarnings(
 )
 
 
-from dotenv import load_dotenv
-from pathlib import Path
-from config.settings import Settings
-from config.logging_config import setup_logging
-from agent.runtime import AgentRuntime
-from agent.memory import InMemoryConversationStore
-from agent.agent import Agent
-from interface import start_api
 
-import os 
-import logging
-import google.auth.exceptions
+#we should load env before importing files (calling main)
+logger = logging.getLogger("main")
+logger.info("loading env...")
+env_path = Path(".env")
+if not env_path.exists():
+    raise FileNotFoundError(f".env file not found at {env_path}")
+load_dotenv(dotenv_path=env_path, override=True)
 
 
 
 
 def main():
-    logger.info("loading env...")
-    env_path = Path(".env")
-    if not env_path.exists():
-        raise FileNotFoundError(f".env file not found at {env_path}")
-    load_dotenv(dotenv_path=env_path, override=True)
+
+    from config.settings import Settings
+    from config.logging_config import setup_logging
+    from agent.runtime import AgentRuntime
+    from agent.memory import InMemoryConversationStore
+    from agent.agent import Agent
+    from interface import start_api
+
+    import os 
 
     os.makedirs("logs", exist_ok = True)
     settings = Settings()
@@ -57,7 +63,6 @@ def main():
 
 
 if __name__ == "__main__":
-    logger = logging.getLogger("main")
     try:
         main()
     except KeyboardInterrupt: 
